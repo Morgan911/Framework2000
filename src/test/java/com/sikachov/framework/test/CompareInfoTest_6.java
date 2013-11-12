3fd9e04770c9f3230b9f984455fda070f8374b26
Morgan <sergey_sikachev@ukr.net>
2013-11-12 18:18:22 +0200

    sdfsdf dfsdfsdf sdfsdfs

--- src/test/java/com/sikachov/framework/test/CompareInfoTest_6.java
+++ src/test/java/com/sikachov/framework/test/CompareInfoTest_6.java
@@ -4,12 +4,14 @@ import org.testng.annotations.Test;
 
 import com.sikachov.framework.helpers.CompareHelper;
 import com.sikachov.framework.helpers.TestDataProvider;
+import com.sikachov.framework.helpers.screenshot.ScreenShotOnFailure;
 
 public class CompareInfoTest_6 extends BaseTest{
 	
 	
 	@Test(dataProvider = "catProvider", dataProviderClass = TestDataProvider.class)
 	public void test(String category, String num){
+	    ScreenShotOnFailure s = new ScreenShotOnFailure();
 		CompareHelper.veryfyTextOnPages(driver,category, num);
 	}
 	
